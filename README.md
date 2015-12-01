23fbfe1d4f82c26ea95199f54b69cd306b586cb3
Navjot Billing <navjotbilling@TyeIsGod.local>
2015-11-30 19:15:38 -0500

    Changes

--- README.md
+++ README.md
@@ -7,4 +7,10 @@ Navjot Billing 100507545
 Paul Azevedo   100461439
 
 ## Description
-You shall not pass!!!  
+
+Pothole founder 
+
+The Pothole founder app is a great way of locating pot holes on the pathway. If a user encounters a pothole on the way, the bump on the road will vibrate the phone and this vibratio on the phone will notify the user of the pothole and store the data into the database. Along with storing the location on the database, it also pinpoints the location of potholes on the map and every pothole found updates the map everytime. 
+
+For the ease of use we have also decided to include a tutorial video for the title page which is fetched from the internet in .3gp format.
+
--- app/src/main/java/com/example/navjotbilling/mdfinalproject/MainActivity.java
+++ app/src/main/java/com/example/navjotbilling/mdfinalproject/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -16,132 +17,85 @@ import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
-    //     Declare variables
+
+    /** Declaration of variables */
     ProgressDialog pDialog;
     VideoView videoview;
-
-    // Insert your Video URL
     String VideoURL = "";
-    private SensorManager mSensorManager;
-    public float mAccel; // acceleration apart from gravity
-    private float mAccelCurrent; // current acceleration including gravity
-    private float mAccelLast; // last acceleration including gravity
 
-    private final SensorEventListener mSensorListener = new SensorEventListener() {
+    /** Intent for the next activity */
 
-        public void onSensorChanged(SensorEvent se) {
-            float x = se.values[0];
-            float y = se.values[1];
-            float z = se.values[2];
-            mAccelLast = mAccelCurrent;
-            mAccelCurrent = (float) Math.sqrt((double) (x*x + y*y + z*z));
-            float delta = mAccelCurrent - mAccelLast;
-            mAccel = mAccel * 0.9f + delta * 0.1f; // perform low-cut filter
-
-            if (mAccel > 2) {
-                    Toast toast = Toast.makeText(getApplicationContext(), "PotHole Found!", Toast.LENGTH_LONG);
-                    toast.show();
-
-                    showNotificaiton("PotHole service", "Pothole Found!");
-
-            }
+private void Continue (){
+    Button about_button=(Button) findViewById(R.id.button);
+    about_button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(MainActivity.this, ShakeTrack.class));
         }
 
+    });
+}
 
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        }
-
-    };
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    protected void onPause() {
-        mSensorManager.unregisterListener(mSensorListener);
-        super.onPause();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-        mAccel = 0.00f;
-        mAccelCurrent = SensorManager.GRAVITY_EARTH;
-        mAccelLast = SensorManager.GRAVITY_EARTH;
-        setContentView(R.layout.activity_main);
-//         Find your VideoView in your video_main.xml layout
-        videoview = (VideoView) findViewById(R.id.videoView);
-        // Execute StreamVideo AsyncTask
 
-        // Create a progressbar
+
+        videoview = (VideoView) findViewById(R.id.videoView);
+
         pDialog = new ProgressDialog(MainActivity.this);
-        // Set progressbar title
-        pDialog.setTitle("Android Video Streaming Tutorial");
-//        // Set progressbar message
         pDialog.setMessage("Buffering...");
         pDialog.setIndeterminate(false);
         pDialog.setCancelable(false);
-//        // Show progressbar
         pDialog.show();
-//
-//            // Start the MediaController
+
+
+        /** Control features for the video player */
+
+
         try{
             MediaController mediacontroller = new MediaController(MainActivity.this);
         mediacontroller.setAnchorView(videoview);
-        // Get the URL from String VideoURL
         Uri video = Uri.parse(VideoURL);
         videoview.setMediaController(mediacontroller);
         videoview.setVideoURI(video);
-        } catch (Exception e) {
+        }
+
+        catch (Exception e) {
             Log.e("Error", e.getMessage());
             e.printStackTrace();
         }
         videoview.requestFocus();
         videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            // Close the progress bar and play the video
             public void onPrepared(MediaPlayer mp) {
                 pDialog.dismiss();
                 videoview.start();
             }
         });
 
+        /** Fetching Uri from Internet resources */
+
         VideoView videoView = (VideoView) findViewById(R.id.videoView);
-        videoView.setMediaController(new android.widget.MediaController(getApplicationContext()));
+        videoView.setMediaController(new android.widget.MediaController(MainActivity.this));
         Uri video = Uri.parse("http://www.androidbegin.com/tutorial/AndroidCommercial.3gp");
         videoView.setVideoURI(video);
         videoView.start();
-    }
 
-
-    private void showNotificaiton(String title, String message) {
-        Intent intent = new Intent(MainActivity.this, MainActivity.class);
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(getApplicationContext());
-        stackBuilder.addParentStack(MainActivity.class);
-        stackBuilder.addNextIntent(intent);
-        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext());
-        mBuilder = new NotificationCompat.Builder(getApplicationContext())
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setAutoCancel(true)
-                .setStyle(new NotificationCompat.BigTextStyle().bigText(message));
-        mBuilder.setContentIntent(resultPendingIntent);
-        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        nm.notify(0, mBuilder.build());
+        Continue();
 
     }
+
 
 }
